02e6136c51c80cae86695d7894e2e93ae5958821
GargiPandeyy <computercomcon@gmail.com>
2025-10-25 01:47:40 +0530

    create path for enemies

--- game.js
+++ game.js
@@ -12,6 +12,16 @@ const gridSize = 20; // 20x20 grid
 const cellWidth = canvas.width / gridSize;
 const cellHeight = canvas.height / gridSize;
 
+// path variables
+const path = [
+    { x: 0, y: 10 },   // start point
+    { x: 5, y: 10 },
+    { x: 5, y: 5 },
+    { x: 15, y: 5 },
+    { x: 15, y: 15 },
+    { x: 19, y: 15 }   // end point
+];
+
 // initialize game
 function init() {
     console.log('game initialized');
@@ -78,6 +88,38 @@ function drawGrid() {
     }
 }
 
+// draw the enemy path
+function drawPath() {
+    ctx.strokeStyle = '#8B4513'; // brown color
+    ctx.lineWidth = 8;
+    ctx.lineCap = 'round';
+    
+    // draw path lines
+    for (let i = 0; i < path.length - 1; i++) {
+        const start = gridToPixel(path[i].x, path[i].y);
+        const end = gridToPixel(path[i + 1].x, path[i + 1].y);
+        
+        ctx.beginPath();
+        ctx.moveTo(start.x + cellWidth/2, start.y + cellHeight/2);
+        ctx.lineTo(end.x + cellWidth/2, end.y + cellHeight/2);
+        ctx.stroke();
+    }
+    
+    // draw start point
+    const startPos = gridToPixel(path[0].x, path[0].y);
+    ctx.fillStyle = '#228B22'; // green for start
+    ctx.beginPath();
+    ctx.arc(startPos.x + cellWidth/2, startPos.y + cellHeight/2, 8, 0, 2 * Math.PI);
+    ctx.fill();
+    
+    // draw end point
+    const endPos = gridToPixel(path[path.length - 1].x, path[path.length - 1].y);
+    ctx.fillStyle = '#DC143C'; // red for end
+    ctx.beginPath();
+    ctx.arc(endPos.x + cellWidth/2, endPos.y + cellHeight/2, 8, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
 // main game loop
 function gameLoop(currentTime) {
     // calculate fps
@@ -91,6 +133,9 @@ function gameLoop(currentTime) {
     
     // draw grid
     drawGrid();
+    
+    // draw path
+    drawPath();
     
     // draw fps counter
     ctx.fillStyle = 'white';
